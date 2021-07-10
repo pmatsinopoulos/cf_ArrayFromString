319e4904214168bcfbb44cc4f78632e9ee71c631
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-10 12:07:00 +0300

    Now using the 'CFArrayApplyFunction()'

--- ArrayFromString/main.c
+++ ArrayFromString/main.c
@@ -6,9 +6,45 @@
 //
 
 #include <stdio.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+void applier(const void *value, void *context) {
+  CFStringRef s = (CFStringRef)value;
+  CFRetain(s);
+  
+  const char *cp = CFStringGetCStringPtr(s, CFStringGetSystemEncoding());
+  
+  if (cp) {
+    printf("%s\n", cp);
+  }
+  else {
+    CFIndex bufferSize = sizeof(char) * (CFStringGetLength(s) + 1);
+    char *buffer = malloc(bufferSize);
+    memset(buffer, 0, bufferSize);
+    
+    CFStringGetCString(s, buffer, bufferSize, CFStringGetSystemEncoding());
+    
+    printf("%s\n", buffer);
+    
+    free(buffer);
+  }
+  
+  CFRelease(s);
+}
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  CFStringRef stringToParse = CFSTR("John Pappas,Mary Foo,Peter Pan");
+  CFStringRef separatorString = CFSTR(",");
+  
+  CFArrayRef array = CFStringCreateArrayBySeparatingStrings(kCFAllocatorDefault, stringToParse, separatorString);
+  
+  CFArrayApplyFunction(array,
+                       CFRangeMake((CFIndex)0,
+                                   (CFIndex)CFArrayGetCount(array)),
+                       applier,
+                       NULL);
+    
+  CFRelease(array);
+  
   return 0;
 }
