d561e70868131d4e1b2de98011d9a451669cf11e
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-10 08:56:17 +0300

    Initial Commit

---
+++ ArrayFromString/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ArrayFromString
+//
+//  Created by Panayotis Matsinopoulos on 10/7/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
